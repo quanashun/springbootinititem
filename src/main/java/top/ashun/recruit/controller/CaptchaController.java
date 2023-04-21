330825ceaa6461ba94c465ee08c1fe37d2961fb4
qks <1848350178@qq.com>
2023-04-21 22:58:43 +0800

    优化

--- src/main/java/top/ashun/recruit/controller/CaptchaController.java
+++ src/main/java/top/ashun/recruit/controller/CaptchaController.java
@@ -3,9 +3,12 @@ package top.ashun.recruit.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.ashun.recruit.config.BusinessException;
+import top.ashun.recruit.pojo.vo.Code;
 import top.ashun.recruit.pojo.vo.R;
 import top.ashun.recruit.util.captcha.ImageCaptchaUtil;
 
@@ -18,6 +21,8 @@ import top.ashun.recruit.util.captcha.ImageCaptchaUtil;
 public class CaptchaController {
     @Autowired
     private ImageCaptchaUtil imageCaptchaUtil;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/getImageCaptcha")
     @ApiOperation("获取图像验证码")
@@ -27,7 +32,22 @@ public class CaptchaController {
 
     @GetMapping("/verifyImageCaptcha")
     @ApiOperation("验证图像验证码")
-    public R verifyImageCaptcha(String captchaId,String captcha) {
-        return R.success(imageCaptchaUtil.verifyCaptcha(captchaId,captcha));
+    public R verifyImageCaptcha(String captchaId, String captcha) {
+        // 获取验证码与redis中存储验证码进行比较
+        String redisCaptcha = (String) redisTemplate.opsForValue().get(captchaId);
+        if (redisCaptcha == null) {
+            // 验证码失效
+            BusinessException.error(Code.USER_CAPTCHA_EXPIRE);
+            return null;
+        }
+        if (redisCaptcha.equals(captcha)) {
+            // 验证码匹配成功
+            redisTemplate.delete(captchaId);
+            return R.success(true);
+        } else {
+            // 验证码匹配失败
+            BusinessException.error(Code.USER_CAPTCHA_ERROR);
+            return null;
+        }
     }
 }
